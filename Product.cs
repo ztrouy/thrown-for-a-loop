47bdab0b33f79aa8d448ad1a97f01787572c7f09
Zachary Trouy <ztrouy@gmail.com>
2024-04-07 19:43:06 -0500

    Updated Product class to add SoldOnDate and TimeInStock properties

--- Product.cs
+++ Product.cs
@@ -3,8 +3,16 @@ using System.Dynamic;
 public class Product {
     public string Name {get; set;}
     public decimal Price {get; set;}
-    public bool Sold {get; set;}
+    public DateTime? SoldOnDate {get; set;}
     public DateTime StockDate {get; set;}
+    public TimeSpan TimeInStock 
+    {
+        get
+        {
+            DateTime lastDay = SoldOnDate != null ? (DateTime)SoldOnDate : DateTime.Now;
+            return lastDay - StockDate;
+        }
+    }
     public int ManufactureYear {get; set;}
     public double Condition {get; set;}
 }
